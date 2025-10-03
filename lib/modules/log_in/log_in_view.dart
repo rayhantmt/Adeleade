a1f8c79c0de92625c696629fa34309678138f5b4
rayhantmt <rayhantmt@gmail.com>
2025-10-04 01:24:03 +0600

    added the container for login screnn

--- lib/modules/log_in/log_in_view.dart
+++ lib/modules/log_in/log_in_view.dart
@@ -15,16 +15,23 @@ class LogInView extends StatelessWidget {
           width: double.infinity,
           fit: BoxFit.cover,
           ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                height: Get.height*0.7,
-                width: Get.width*0.9,
-                color: Colors.white,
-              )
-            ],
+          Padding(
+            padding: const EdgeInsets.only(left: 20,right: 20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.white,
+                  ),
+                  height: Get.height*0.7,
+                  width: Get.width*0.9,
+                  
+                )
+              ],
+            ),
           )
         ],
       ),
