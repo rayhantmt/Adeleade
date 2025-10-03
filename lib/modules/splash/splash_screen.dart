08f8413b290280646d9f945666c56e28d41a81eb
rayhantmt <rayhantmt@gmail.com>
2025-10-04 00:48:51 +0600

    done with splahs screen

--- lib/modules/splash/splash_screen.dart
+++ lib/modules/splash/splash_screen.dart
@@ -9,13 +9,74 @@ class SplashScreen extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
-          Image.asset(AppImages.amination,
-          height: double.infinity,
-          width: double.infinity,
-          fit: BoxFit.cover,
+          Image.asset(
+            AppImages.amination,
+            fit: BoxFit.fill,
+            width: double.infinity,
+            height: double.infinity,
           ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(width: double.infinity),
+              Text(
+                "Momentum",
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 32,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(height: 200),
+              Container(
+                height: 60,
+                width: 350,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(99),
+                  color: Color(0xffDACBA4),
+                ),
+                child: Center(
+                  child: Text(
+                    'Request an invite',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 14,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 30),
+              Container(
+                height: 60,
+                width: 350,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(99),
+                  color: Colors.white,
+                ),
+                child: Center(
+                  child: Text(
+                    'Log In',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 14,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          // Column(
+          //   mainAxisAlignment: MainAxisAlignment.center,
+          //   crossAxisAlignment: CrossAxisAlignment.center,
+          //   children: [
+          //
+          //   ],
+          // )
         ],
-      )
+      ),
     );
   }
 }
