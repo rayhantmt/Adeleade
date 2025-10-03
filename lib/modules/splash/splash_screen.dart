cf5f15c8532db1c246108a20aa8999c05a260205
rayhantmt <rayhantmt@gmail.com>
2025-10-04 00:38:42 +0600

    added properly the gif file starting the full project

--- lib/modules/splash/splash_screen.dart
+++ lib/modules/splash/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mementum/utils/app_images.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
@@ -6,7 +7,7 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Image.asset('assets/giphy.gif')
+      body: Image.asset(AppImages.amination)
     );
   }
 }
