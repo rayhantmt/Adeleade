ed7fc562a7966a1ea53db85065dd327cd1d7e24a
rayhantmt <rayhantmt@gmail.com>
2025-10-05 01:43:58 +0600

    done with forgot password

--- lib/modules/forgot_password/forgot_password_view.dart
+++ lib/modules/forgot_password/forgot_password_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:mementum/common_widgets/textfield.dart';
 import 'package:mementum/utils/app_colors.dart';
 
 class ForgotPasswordView extends StatelessWidget {
@@ -9,54 +10,68 @@ class ForgotPasswordView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xffFAF2ED),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          SizedBox(width: double.infinity,),
-           Text(
-                "Momentum",
+      body: Padding(
+        padding: const EdgeInsets.only(left: 20,right: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(width: double.infinity,),
+             Text(
+                  "Momentum",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 32,
+                    color: Color(0xff1E1E1E),
+                  ),
+                ),
+                Text('Forgot password',
                 style: TextStyle(
                   fontWeight: FontWeight.w700,
-                  fontSize: 32,
-                  color: Color(0xff1E1E1E),
+                  fontSize: 26,
+                  color: Colors.black
                 ),
-              ),
-              Text('Forgot password',
-              style: TextStyle(
-                fontWeight: FontWeight.w700,
-                fontSize: 26,
-                color: Colors.black
-              ),
-              ),
-              Text('Enter your email account to reset  your password',
-              maxLines: 6,
-              style: TextStyle(
-                fontWeight: FontWeight.w400,
-                fontSize: 16,
-                color: Color(0xff7D848D)
-              ),
-              ),
-              SizedBox(height: Get.height * 0.033),
-                      Container(
-                        height: Get.height * 0.07,
-                        width: Get.width * 0.8,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(99),
-                          color: AppColors.primarycolor,
+                ),
+                Text('Enter your email account to reset  your password',
+                maxLines: 6,
+                style: TextStyle(
+                  fontWeight: FontWeight.w400,
+                  fontSize: 16,
+                  color: Color(0xff7D848D)
+                ),
+                ),
+                SizedBox(height: Get.height * 0.033),
+                       
+                        Padding(
+                          padding: EdgeInsets.only(left: Get.width * 0.1),
+                          child: Row(children: [Text('Email')]),
                         ),
-                        child: Center(
-                          child: Text(
-                            'Forgot password',
-                            style: TextStyle(
-                              fontWeight: FontWeight.w600,
-                              fontSize: 14,
-                              color: Colors.black,
+                        SizedBox(height: Get.height*0.01,),
+                        Commontextfield(
+                          tittle: 'Rayhantmt@gmail.com',
+                          obsecuretext: false,
+                        ),
+                        SizedBox(height: Get.height * 0.033),
+                         Container(
+                          height: Get.height * 0.07,
+                          width: Get.width * 0.8,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(99),
+                            color: AppColors.primarycolor,
+                          ),
+                          child: Center(
+                            child: Text(
+                              'Forgot password',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                fontSize: 14,
+                                color: Colors.black,
+                              ),
                             ),
                           ),
                         ),
-                      ),
-        ],
+          ],
+        ),
       ),
     );
   }
