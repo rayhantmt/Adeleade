69c2159647f5a0a32f6cf950ff72cb829e525e69
rayhantmt <rayhantmt@gmail.com>
2025-10-05 02:34:25 +0600

    added the logo on the screen

--- lib/modules/update_password/update_password_view.dart
+++ lib/modules/update_password/update_password_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:mementum/utils/app_colors.dart';
 
 class UpdatePasswordView extends StatelessWidget {
   const UpdatePasswordView({super.key});
@@ -7,6 +9,25 @@ class UpdatePasswordView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor:  Color(0xffFAF2ED),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 20,right: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(width: double.infinity,),
+             Text(
+                  "Momentum",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 32,
+                    color: AppColors.primarycolor,
+                  ),
+                ),
+                SizedBox(height: Get.height * 0.033),
+          ],
+        ),
+      ),
     );
   }
 }
