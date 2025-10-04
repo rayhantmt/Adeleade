5a16ad9341ce0522bd14af8cb9bb06f3f923fdc5
rayhantmt <rayhantmt@gmail.com>
2025-10-05 03:23:08 +0600

    added the update password successful screen

---
+++ lib/modules/update_password_successful/update_password_successfull.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class UpdatePasswordSuccessfull extends StatelessWidget {
+  const UpdatePasswordSuccessfull({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
