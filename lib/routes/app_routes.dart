596c9cfc8ae1a2b4fcb24de67fd8f2e1165a148a
rayhantmt <rayhantmt@gmail.com>
2025-10-05 05:23:28 +0600

    added the binding to routes

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -3,6 +3,7 @@ import 'package:mementum/modules/forgot_password/forgot_password_view.dart';
 import 'package:mementum/modules/log_in/log_in_binding.dart';
 import 'package:mementum/modules/log_in/log_in_view.dart';
 import 'package:mementum/modules/main_screen/main_screen.dart';
+import 'package:mementum/modules/main_screen/main_screen_binding.dart';
 import 'package:mementum/modules/otp_page/otp_page_binding.dart';
 import 'package:mementum/modules/otp_page/otp_page_view.dart';
 import 'package:mementum/modules/request_an_invite/requesst_an_invite_binding.dart';
@@ -20,6 +21,6 @@ class AppRoutes {
     GetPage(name: AppPages.otppage, page: () => OtpPageView(),binding: OtpPageBinding()),
     GetPage(name: AppPages.updatepassword, page: () => UpdatePasswordView(),binding: UpdatePasswordBinding()),
     GetPage(name: AppPages.updatePasswordSuccessfull, page: () => UpdatePasswordSuccessfull(),),
-    GetPage(name: AppPages.mainscreen, page: () => MainScreen(),)
+    GetPage(name: AppPages.mainscreen, page: () => MainScreen(),binding: MainScreenBinding())
   ];
 }
