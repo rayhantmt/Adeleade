ac22484b3317948ccc94fc93c461fddfabeed018
rayhantmt <rayhantmt@gmail.com>
2025-10-05 03:40:23 +0600

    added the memories module

---
+++ lib/modules/memories/memories_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MemoriesView extends StatelessWidget {
+  const MemoriesView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Memories'),
+      ),
+    );
+  }
+}
