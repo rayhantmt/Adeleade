5baadaa874a4238d75423e52017429f13c4ba9e4
rayhantmt <rayhantmt@gmail.com>
2025-10-05 01:51:39 +0600

    added navigation of otp page

--- lib/modules/forgot_password/forgot_password_view.dart
+++ lib/modules/forgot_password/forgot_password_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mementum/common_widgets/textfield.dart';
+import 'package:mementum/routes/app_pages.dart';
 import 'package:mementum/utils/app_colors.dart';
 
 class ForgotPasswordView extends StatelessWidget {
@@ -54,24 +55,27 @@ class ForgotPasswordView extends StatelessWidget {
                           obsecuretext: false,
                         ),
                         SizedBox(height: Get.height * 0.033),
-                         Container(
-                          height: Get.height * 0.07,
-                          width: Get.width * 0.8,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(99),
-                            color: AppColors.primarycolor,
-                          ),
-                          child: Center(
-                            child: Text(
-                              'Forgot password',
-                              style: TextStyle(
-                                fontWeight: FontWeight.w600,
-                                fontSize: 14,
-                                color: Colors.black,
+                         GestureDetector(
+                          onTap: () => Get.toNamed(AppPages.otppage),
+                           child: Container(
+                            height: Get.height * 0.07,
+                            width: Get.width * 0.8,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(99),
+                              color: AppColors.primarycolor,
+                            ),
+                            child: Center(
+                              child: Text(
+                                'Send otp',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 14,
+                                  color: Colors.black,
+                                ),
                               ),
                             ),
-                          ),
-                        ),
+                                                   ),
+                         ),
           ],
         ),
       ),
--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -2,4 +2,5 @@ class AppPages {
   static const String login='/login';
   static const String signup='/signup';
   static const String forgotpassword='/forgotpassword';
+  static const String otppage='/otppage';
 }
