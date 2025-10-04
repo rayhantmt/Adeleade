88fe8e309a419b5711d81f52e1ea0fbb8f8a0ce0
rayhantmt <rayhantmt@gmail.com>
2025-10-05 01:24:48 +0600

    designing the forgot password screen

--- lib/modules/forgot_password/forgot_password_view.dart
+++ lib/modules/forgot_password/forgot_password_view.dart
@@ -7,6 +7,35 @@ class ForgotPasswordView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xffFAF2ED),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(width: double.infinity,),
+           Text(
+                "Momentum",
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 32,
+                  color: Color(0xff1E1E1E),
+                ),
+              ),
+              Text('Forgot password',
+              style: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 26,
+                color: Colors.black
+              ),
+              ),
+              Text('Enter your email account to reset  your password',
+              style: TextStyle(
+                fontWeight: FontWeight.w400,
+                fontSize: 16,
+                color: Color(0xff7D848D)
+              ),
+              )
+        ],
+      ),
     );
   }
 }
