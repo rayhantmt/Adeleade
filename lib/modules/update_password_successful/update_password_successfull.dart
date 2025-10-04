a7eb1c3b0d923ea974165ec139b9de20933d17eb
rayhantmt <rayhantmt@gmail.com>
2025-10-05 03:26:09 +0600

    done with the congratulations screen

--- lib/modules/update_password_successful/update_password_successfull.dart
+++ lib/modules/update_password_successful/update_password_successfull.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:mementum/utils/app_colors.dart';
 
 class UpdatePasswordSuccessfull extends StatelessWidget {
   const UpdatePasswordSuccessfull({super.key});
@@ -6,7 +8,63 @@ class UpdatePasswordSuccessfull extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor:  Color(0xffFAF2ED),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 20,right: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(width: double.infinity,),
+             Text(
+                  "Momentum",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 32,
+                    color: AppColors.primarycolor,
+                  ),
+                ),
+                SizedBox(height: Get.height * 0.033),
+                Text('Congratulations',
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 26,
+                  color: Colors.black
+                ),
+                ),
+                 SizedBox(height: Get.height * 0.033),
+                 Text('Your password has been successfully reset',
+                 style: TextStyle(
+                  fontWeight: FontWeight.w400,
+                  fontSize: 16,
+                  color: Color(0xff7D848D)
+                 ),
+                 ),
+                  SizedBox(height: Get.height * 0.02),
+                      
+                     
+                      SizedBox(height: Get.height * 0.033),
+                      Container(
+                        height: Get.height * 0.07,
+                        width: Get.width * 0.8,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(99),
+                          color: AppColors.primarycolor,
+                        ),
+                        child: Center(
+                          child: Text(
+                            'Confirm',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 14,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                      ),
+          ],
+        ),
+      ),
     );
   }
 }
