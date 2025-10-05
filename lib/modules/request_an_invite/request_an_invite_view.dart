06a7b2c781a1769101ff01c2b1c3a2f9c521cfbd
rayhantmt <rayhantmt@gmail.com>
2025-10-06 04:46:49 +0600

    added a singlechild scrollview on the request an invite page

--- lib/modules/request_an_invite/request_an_invite_view.dart
+++ lib/modules/request_an_invite/request_an_invite_view.dart
@@ -32,104 +32,106 @@ class RequestAnInviteView extends GetView<RequestAnInviteController> {
                   ),
                   height: Get.height * 0.6,
                   width: Get.width * 0.9,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      SizedBox(height: Get.height * 0.02),
-                      Text(
-                        'Request an invite',
-                        style: TextStyle(
-                          fontWeight: FontWeight.w700,
-                          fontSize: 26,
-                          color: AppColors.primarycolor,
-                        ),
-                      ),
-                      SizedBox(height: Get.height * 0.02),
-                      Text(
-                        'Create an account or log in to explore our app',
-                        style: TextStyle(
-                          fontWeight: FontWeight.w400,
-                          fontSize: 12,
-                          color: Color(0xff6C7278),
-                        ),
-                      ),
-                      SizedBox(height: Get.height * 0.02),
-                      Padding(
-                        padding: EdgeInsets.only(left: Get.width * 0.1),
-                        child: Row(children: [Text('Name')]),
-                      ),
-                      Commontextfield(
-                        tittle: 'Rayhan',
-                        obsecuretext: false,
-                      ),
-                      SizedBox(height: Get.height * 0.02),
-                      Padding(
-                        padding: EdgeInsets.only(left: Get.width * 0.1),
-                        child: Row(children: [Text('Email')]),
-                      ),
-                      Commontextfield(
-                        tittle: 'Rayhantmt@gmail.com',
-                        obsecuretext: false,
-                      ),
-                      SizedBox(height: Get.height * 0.02),
-                      Padding(
-                        padding: EdgeInsets.only(left: Get.width * 0.1),
-                        child: Row(children: [Text('Password')]),
-                      ),
-                      Obx(
-                        () => Commontextfield(
-                          tittle: '*******',
-                          obsecuretext: controller.isObscured.value,
-                          icon: GestureDetector(
-                            onTap: controller.toggleObscureText,
-                            child: controller.isObscured.value
-                                ? Icon(Icons.visibility)
-                                : Icon(Icons.visibility_off),
+                  child: SingleChildScrollView(
+                    child: Column(
+                     // mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        SizedBox(height: Get.height * 0.02),
+                        Text(
+                          'Request an invite',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 26,
+                            color: AppColors.primarycolor,
                           ),
                         ),
-                      ),
-                       SizedBox(height: Get.height * 0.02),
-                      Padding(
-                        padding: EdgeInsets.only(left: Get.width * 0.1),
-                        child: Row(children: [Text('Confirm Password')]),
-                      ),
-                      Obx(
-                        () => Commontextfield(
-                          tittle: '*******',
-                          obsecuretext: controller.isObscured1.value,
-                          icon: GestureDetector(
-                            onTap: controller.toggleObscureText1,
-                            child: controller.isObscured1.value
-                                ? Icon(Icons.visibility)
-                                : Icon(Icons.visibility_off),
+                        SizedBox(height: Get.height * 0.02),
+                        Text(
+                          'Create an account or log in to explore our app',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                            fontSize: 12,
+                            color: Color(0xff6C7278),
                           ),
                         ),
-                      ),
-                      
-                     
-                      SizedBox(height: Get.height * 0.033),
-                      Container(
-                        height: Get.height * 0.07,
-                        width: Get.width * 0.8,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(99),
-                          color: AppColors.primarycolor,
+                        SizedBox(height: Get.height * 0.02),
+                        Padding(
+                          padding: EdgeInsets.only(left: Get.width * 0.1),
+                          child: Row(children: [Text('Name')]),
                         ),
-                        child: Center(
-                          child: Text(
-                            'Request an invite',
-                            style: TextStyle(
-                              fontWeight: FontWeight.w600,
-                              fontSize: 14,
-                              color: Colors.black,
+                        Commontextfield(
+                          tittle: 'Rayhan',
+                          obsecuretext: false,
+                        ),
+                        SizedBox(height: Get.height * 0.02),
+                        Padding(
+                          padding: EdgeInsets.only(left: Get.width * 0.1),
+                          child: Row(children: [Text('Email')]),
+                        ),
+                        Commontextfield(
+                          tittle: 'Rayhantmt@gmail.com',
+                          obsecuretext: false,
+                        ),
+                        SizedBox(height: Get.height * 0.02),
+                        Padding(
+                          padding: EdgeInsets.only(left: Get.width * 0.1),
+                          child: Row(children: [Text('Password')]),
+                        ),
+                        Obx(
+                          () => Commontextfield(
+                            tittle: '*******',
+                            obsecuretext: controller.isObscured.value,
+                            icon: GestureDetector(
+                              onTap: controller.toggleObscureText,
+                              child: controller.isObscured.value
+                                  ? Icon(Icons.visibility)
+                                  : Icon(Icons.visibility_off),
                             ),
                           ),
                         ),
-                      ),
-                     
-                      
-                    ],
+                         SizedBox(height: Get.height * 0.02),
+                        Padding(
+                          padding: EdgeInsets.only(left: Get.width * 0.1),
+                          child: Row(children: [Text('Confirm Password')]),
+                        ),
+                        Obx(
+                          () => Commontextfield(
+                            tittle: '*******',
+                            obsecuretext: controller.isObscured1.value,
+                            icon: GestureDetector(
+                              onTap: controller.toggleObscureText1,
+                              child: controller.isObscured1.value
+                                  ? Icon(Icons.visibility)
+                                  : Icon(Icons.visibility_off),
+                            ),
+                          ),
+                        ),
+                        
+                       
+                        SizedBox(height: Get.height * 0.033),
+                        Container(
+                          height: Get.height * 0.07,
+                          width: Get.width * 0.8,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(99),
+                            color: AppColors.primarycolor,
+                          ),
+                          child: Center(
+                            child: Text(
+                              'Request an invite',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                fontSize: 14,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                        ),
+                       
+                        
+                      ],
+                    ),
                   ),
                 ),
               ],
