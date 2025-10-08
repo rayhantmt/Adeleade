e50ac34b4a6e476e681dcf0fde40e04eced18fee
rayhantmt <rayhantmt@gmail.com>
2025-10-08 06:22:44 +0600

    added the home screen search container

--- lib/modules/home/home_view.dart
+++ lib/modules/home/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
+import 'package:mementum/common_widgets/textfield.dart';
 
 import 'package:mementum/utils/app_images.dart';
 
@@ -18,31 +19,43 @@ class HomeView extends StatelessWidget {
               width: double.infinity,
               fit: BoxFit.cover,
             ),
-            Column(
-              children: [
-                SizedBox(height: Get.height*0.07,),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Image.asset(
-                      AppImages.logo,
-                      height: 40,
-                      width: 40,
-                      color: Colors.black,
+            Padding(
+              padding: const EdgeInsets.only(left: 20,right: 20),
+              child: Column(
+                children: [
+                  SizedBox(height: Get.height*0.07,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Image.asset(
+                        AppImages.logo,
+                        height: 40,
+                        width: 40,
+                        color: Colors.black,
+                      ),
+                      Text('Momentum',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                        color: Colors.black,
+                        fontSize: 24
+                      ),
+                      ),
+                      Icon(Icons.circle_notifications_outlined,
+                      size: 40,
+                      )
+                    ],
+                  ),
+                  Container(
+                    height: Get.height*0.07,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8)
                     ),
-                    Text('Momentum',
-                    style: TextStyle(
-                      fontWeight: FontWeight.w700,
-                      color: Colors.black,
-                      fontSize: 24
-                    ),
-                    ),
-                    Icon(Icons.circle_notifications_outlined,
-                    size: 40,
-                    )
-                  ],
-                ),
-              ],
+                    child: Commontextfield(tittle: "Search", obsecuretext: false,icon: Icon(Icons.search),)
+                  )
+                ],
+              ),
             ),
           ],
         ),
