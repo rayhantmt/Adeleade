fb67f2c5e2a6101a475fe1709a50cab926bf9c01
rayhantmt <rayhantmt@gmail.com>
2025-10-08 23:19:19 +0600

    removed the unused expanded widget

--- lib/modules/home/home_view.dart
+++ lib/modules/home/home_view.dart
@@ -10,55 +10,53 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Expanded(
-        child: Stack(
-          children: [
-            Image.asset(
-              AppImages.background,
-              height: double.infinity,
-              width: double.infinity,
-              fit: BoxFit.cover,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 20,right: 20),
-              child: Column(
-                children: [
-                  SizedBox(height: Get.height*0.07,),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Image.asset(
-                        AppImages.logo,
-                        height: 40,
-                        width: 40,
-                        color: Colors.black,
-                      ),
-                      Text('Momentum',
-                      style: TextStyle(
-                        fontWeight: FontWeight.w700,
-                        color: Colors.black,
-                        fontSize: 24
-                      ),
-                      ),
-                      Icon(Icons.circle_notifications_outlined,
-                      size: 40,
-                      )
-                    ],
-                  ),
-                  Container(
-                    height: Get.height*0.07,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(8)
+      body: Stack(
+        children: [
+          Image.asset(
+            AppImages.background,
+            height: double.infinity,
+            width: double.infinity,
+            fit: BoxFit.cover,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 20,right: 20),
+            child: Column(
+              children: [
+                SizedBox(height: Get.height*0.07,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Image.asset(
+                      AppImages.logo,
+                      height: 40,
+                      width: 40,
+                      color: Colors.black,
                     ),
-                    child: Commontextfield(tittle: "Search", obsecuretext: false,icon: Icon(Icons.search),)
-                  )
-                ],
-              ),
+                    Text('Momentum',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w700,
+                      color: Colors.black,
+                      fontSize: 24
+                    ),
+                    ),
+                    Icon(Icons.circle_notifications_outlined,
+                    size: 40,
+                    )
+                  ],
+                ),
+                Container(
+                  height: Get.height*0.07,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(8)
+                  ),
+                  child: Commontextfield(tittle: "Search", obsecuretext: false,icon: Icon(Icons.search),)
+                )
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
