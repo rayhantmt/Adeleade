b30768b3f62b1f2cd7ddc1f66edaf126a016584d
rayhantmt <rayhantmt@gmail.com>
2025-10-09 01:58:16 +0600

    succesfully got the model data from the controller

--- lib/modules/main_screen/main_screen_binding.dart
+++ lib/modules/main_screen/main_screen_binding.dart
@@ -1,10 +1,12 @@
 
 import 'package:get/get.dart';
+import 'package:mementum/modules/home/home_controller.dart';
 import 'package:mementum/modules/main_screen/main_screen_controller.dart';
 
 class MainScreenBinding extends Bindings{
   @override
   void dependencies() {
    Get.lazyPut<MainScreenController>(() => MainScreenController(),);
+    Get.lazyPut<HomeController>(() => HomeController(),);
   }
 }
