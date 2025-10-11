05173f2bef7c529f228f11ac706d8db1e171c5b1
rayhantmt <rayhantmt@gmail.com>
2025-10-11 23:10:19 +0600

    removed an unused import

--- lib/modules/home/home_view.dart
+++ lib/modules/home/home_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
 import 'package:mementum/common_widgets/textfield.dart';
 import 'package:mementum/modules/home/home_controller.dart';
