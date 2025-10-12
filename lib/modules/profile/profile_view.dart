feb2d65e18febca1fb67e7c55ffc2cae3f18e811
rayhantmt <rayhantmt@gmail.com>
2025-10-13 03:16:44 +0600

    added the back icon

--- lib/modules/profile/profile_view.dart
+++ lib/modules/profile/profile_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mementum/utils/app_images.dart';
 
 class ProfileView extends StatelessWidget {
   const ProfileView({super.key});
@@ -8,7 +9,11 @@ class ProfileView extends StatelessWidget {
     return Scaffold(
       backgroundColor: Color(0xffF9F7F1),
       body: SafeArea(child: Column(
-
+children: [
+  Row(children: [
+    Image.asset(AppImages.back_icon)
+  ],)
+],
       )),
     );
   }
--- lib/utils/app_images.dart
+++ lib/utils/app_images.dart
@@ -15,4 +15,5 @@ class AppImages {
   static const String calander_icon='assets/calander_icon.png';
   static const String location_icon='assets/location_icon.png';
   static const String notification_logo='assets/notification_logo.png';
+  static const String back_icon='assets/back_icon.png';
 }
