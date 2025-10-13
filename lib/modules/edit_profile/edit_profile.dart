0fe32a730ceac501853966e61277b82ff22b69e9
rayhantmt <rayhantmt@gmail.com>
2025-10-13 08:02:27 +0600

    implemented some thing

--- lib/modules/edit_profile/edit_profile.dart
+++ lib/modules/edit_profile/edit_profile.dart
@@ -12,56 +12,110 @@ class EditProfile extends StatelessWidget {
       body: Stack(
         children: [
           Image.asset(AppImages.background),
-          SafeArea(child: 
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 18),
-            child: Column(
-              children: [
-                SizedBox(
-                  height: Get.height * 0.07,
-                  child: Stack(
-                    children: [
-                      Align(
-                        alignment: AlignmentGeometry.centerLeft,
-                        child: Image.asset(
-                          AppImages.back_icon,
-                          height: Get.height * 0.07,
-                          width: Get.height * 0.07,
-                        ),
-                      ),
-                      Center(
-                        child: Text(
-                          'Edit Profile',
-                          style: GoogleFonts.inter(
-                            fontWeight: FontWeight.w500,
-                            fontSize: 18,
-                            color: Colors.black,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(height: Get.height*0.02,),
-                Stack(
-                   
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 18),
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
                   children: [
-                    ClipRRect(
-                      borderRadius: BorderRadiusGeometry.circular(6),
-                      child: Image.asset(AppImages.profilephoto
-                      )),
-                      Align(
-                        alignment: AlignmentGeometry.center,
-                        child: CircleAvatar(
-                          child: Image.asset(AppImages.profilephoto),
-                        ),
-                      )
+                    SizedBox(
+                      height: Get.height * 0.07,
+                      child: Stack(
+                        children: [
+                          Align(
+                            alignment: AlignmentGeometry.centerLeft,
+                            child: GestureDetector(
+                              onTap: () => Get.back(),
+                              child: Image.asset(
+                                AppImages.back_icon,
+                                height: Get.height * 0.07,
+                                width: Get.height * 0.07,
+                              ),
+                            ),
+                          ),
+                          Center(
+                            child: Text(
+                              'Edit Profile',
+                              style: GoogleFonts.inter(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 18,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: Get.height * 0.02),
+                    Stack(
+  clipBehavior: Clip.none,
+  children: [
+    // Cover Photo
+    AspectRatio(
+      aspectRatio: 16 / 9,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(6),
+        child: Image.asset(
+          AppImages.profilephoto,
+          fit: BoxFit.cover,
+          width: double.infinity,
+        ),
+      ),
+    ),
+    // ✅ Edit Icon for COVER PHOTO (Top Right)
+    Positioned(
+      top: 10,      // 10px from top of cover photo
+      right: 10,    // 10px from right edge
+      child: GestureDetector(
+        onTap: () {
+          // Handle cover photo edit
+          print('Edit cover photo');
+        },
+        child: Container(
+          padding: EdgeInsets.all(8),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(6),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black26,
+                blurRadius: 4,
+                offset: Offset(0, 2),
+              ),
+            ],
+          ),
+          child: Icon(
+            Icons.camera_alt,
+            size: 20,
+            color: Colors.black87,
+          ),
+        ),
+      ),
+    ),
+    // Profile Picture (Overlapping)
+    Positioned(
+      bottom: -Get.height * 0.07,
+      left: Get.width * 0.5 - Get.height * 0.1,
+      child: Container(
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          border: Border.all(color: Colors.white, width: 4),
+        ),
+        child: CircleAvatar(
+          radius: Get.height * 0.07,
+          backgroundColor: Colors.white,
+          backgroundImage: AssetImage(AppImages.back_icon),
+        ),
+      ),
+    ),
+  ],
+)
                   ],
-                )
-              ],
+                ),
+              ),
             ),
-          )
-          )
+          ),
         ],
       ),
     );
