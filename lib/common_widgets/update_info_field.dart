906ba2002be18e7b89b08e2621065721a789a31e
rayhantmt <rayhantmt@gmail.com>
2025-10-13 09:09:34 +0600

    crating the baisc design

---
+++ lib/common_widgets/update_info_field.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class UpdateInfoField extends StatelessWidget {
+  const UpdateInfoField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
