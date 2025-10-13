e77374b938c343e1f8f178531f8117b7a79950ca
rayhantmt <rayhantmt@gmail.com>
2025-10-14 00:58:00 +0600

    added the app bar

--- lib/modules/memories/memories_view.dart
+++ lib/modules/memories/memories_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:mementum/utils/app_images.dart';
 
 
 class MemoriesView extends StatelessWidget {
@@ -8,11 +11,40 @@ class MemoriesView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xffF9F7F1),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-
-          ],
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                     SizedBox(
+                    height: Get.height * 0.07,
+                    child: Stack(
+                      children: [
+                        Align(
+                          alignment: AlignmentGeometry.centerLeft,
+                          child: Image.asset(
+                            AppImages.back_icon,
+                            height: Get.height * 0.07,
+                            width: Get.height * 0.07,
+                          ),
+                        ),
+                        Center(
+                          child: Text(
+                            'My Profile',
+                            style: GoogleFonts.inter(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 18,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+              ],
+            ),
+          ),
         ),
       )
     );
