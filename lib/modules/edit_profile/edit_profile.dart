8a15cdd3231563541deb2497059ae85cf842230b
rayhantmt <rayhantmt@gmail.com>
2025-10-13 07:11:12 +0600

    edit profile

--- lib/modules/edit_profile/edit_profile.dart
+++ lib/modules/edit_profile/edit_profile.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:mementum/utils/app_images.dart';
 
 class EditProfile extends StatelessWidget {
@@ -9,7 +11,57 @@ class EditProfile extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
-          Image.asset(AppImages.background)
+          Image.asset(AppImages.background),
+          SafeArea(child: 
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 18),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: Get.height * 0.07,
+                  child: Stack(
+                    children: [
+                      Align(
+                        alignment: AlignmentGeometry.centerLeft,
+                        child: Image.asset(
+                          AppImages.back_icon,
+                          height: Get.height * 0.07,
+                          width: Get.height * 0.07,
+                        ),
+                      ),
+                      Center(
+                        child: Text(
+                          'Edit Profile',
+                          style: GoogleFonts.inter(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 18,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: Get.height*0.02,),
+                Stack(
+                   
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadiusGeometry.circular(6),
+                      child: Image.asset(AppImages.profilephoto
+                      )),
+                      Align(
+                        alignment: AlignmentGeometry.center,
+                        child: CircleAvatar(
+                          child: Image.asset(AppImages.profilephoto),
+                        ),
+                      )
+                  ],
+                )
+              ],
+            ),
+          )
+          )
         ],
       ),
     );
