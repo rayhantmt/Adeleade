4a851b09a929034e3cc5f5b9fae3b979da6cabb6
rayhantmt <rayhantmt@gmail.com>
2025-10-13 08:37:58 +0600

    created the whole edit profile info section

--- lib/common_widgets/edit_profile_info.dart
+++ lib/common_widgets/edit_profile_info.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class EditProfileInfo extends StatelessWidget {
   final String tittle;
@@ -8,8 +10,38 @@ class EditProfileInfo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(tittle)
+        Text(tittle,
+        style: GoogleFonts.inter(
+          fontWeight: FontWeight.w500,
+          fontSize: 16,
+          color: Colors.black
+        ),
+        ),
+        SizedBox(height: Get.height*0.005,),
+        Container(
+          height: Get.height*0.04,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            border: Border.all(
+              width: 1,
+              color: Color(0xff625B4A)
+            )
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(info,
+            style: GoogleFonts.inter(
+
+              fontWeight: FontWeight.w400,
+              fontSize: 16,
+              color: Colors.black
+            ),
+            ),
+          ),
+        )
       ],
     );
   }
