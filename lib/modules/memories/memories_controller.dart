58fd2bc189e733a4cc42fb55d4ffeb5eb1ea1197
rayhantmt <rayhantmt@gmail.com>
2025-10-14 01:19:38 +0600

    added the toggoling

--- lib/modules/memories/memories_controller.dart
+++ lib/modules/memories/memories_controller.dart
@@ -1,5 +1,9 @@
 import 'package:get/get.dart';
 
 class MemoriesController extends GetxController{
-  
+  var memorytype = 0.obs; // 0 = On-Time, 1 = Recurring Deposit
+
+  void setDepositType(int type) {
+    memorytype.value = type;
+  }
 }
--- lib/modules/memories/memories_view.dart
+++ lib/modules/memories/memories_view.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:mementum/modules/memories/memories_controller.dart';
+import 'package:mementum/utils/app_colors.dart';
 import 'package:mementum/utils/app_images.dart';
 
 
-class MemoriesView extends StatelessWidget {
+class MemoriesView extends GetView<MemoriesController> {
   const MemoriesView({super.key});
 
   @override
@@ -43,9 +45,63 @@ class MemoriesView extends StatelessWidget {
                     ),
                   ),
                   SizedBox(height: Get.height*0.01,),
-                  Container(
-                    
+                  Obx(() => 
+                  Row(
+                    children: [
+                        Expanded(
+                            child: GestureDetector(
+                              onTap: () => controller.setDepositType(0),
+                              child: Container(
+                                padding: EdgeInsets.symmetric(vertical: 12),
+                                decoration: BoxDecoration(
+                                  color: controller.memorytype.value == 0
+                                      ? AppColors.primarycolor
+                                      : Color(0xffE7F0EB),
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                child: Center(
+                                  child: Text(
+                                    "On-Time",
+                                    style: TextStyle(
+                                      color: controller.memorytype.value == 0
+                                          ? Colors.white
+                                          : Color(0xff126535),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(width: 5,),
+                          Expanded(
+                            child: GestureDetector(
+                              onTap: () => controller.setDepositType(1),
+                              child: Container(
+                                padding: EdgeInsets.symmetric(vertical: 12),
+                                decoration: BoxDecoration(
+                                  color: controller.memorytype.value == 1
+                                      ? AppColors.primarycolor
+                                      : Color(0xffE7F0EB),
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                child: Center(
+                                  child: Text(
+                                    "Recurring Deposit",
+                                    style: TextStyle(
+                                      color: controller.memorytype.value == 1
+                                          ? Colors.white
+                                          : Color(0xff126535),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                    ],
                   )
+                  
+                  
+                  ,)
               ],
             ),
           ),
