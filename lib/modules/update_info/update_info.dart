1d3cb482e7841c8967efebe8a9f4336e82f9fb54
rayhantmt <rayhantmt@gmail.com>
2025-10-13 09:06:17 +0600

    added the app bar of update info screen

--- lib/modules/update_info/update_info.dart
+++ lib/modules/update_info/update_info.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:mementum/utils/app_images.dart';
 
 class UpdateInfoView extends StatelessWidget {
@@ -15,10 +17,43 @@ class UpdateInfoView extends StatelessWidget {
           fit: BoxFit.cover,
           ),
           SafeArea(
-            child: Column(
-              children: [
-                  
-              ],
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 18),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                      SizedBox(
+                          height: Get.height * 0.07,
+                          child: Stack(
+                            children: [
+                              Align(
+                                alignment: AlignmentGeometry.centerLeft,
+                                child: GestureDetector(
+                                  onTap: () => Get.back(),
+                                  child: Image.asset(
+                                    AppImages.back_icon,
+                                    height: Get.height * 0.07,
+                                    width: Get.height * 0.07,
+                                  ),
+                                ),
+                              ),
+                              Center(
+                                child: Text(
+                                  'Update Info',
+                                  style: GoogleFonts.inter(
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 18,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(height: Get.height * 0.02),
+                  ],
+                ),
+              ),
             ),
           )
         ],
