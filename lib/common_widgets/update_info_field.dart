efd3431d36f3a3d0f428f1672431cc9257d48e87
rayhantmt <rayhantmt@gmail.com>
2025-10-13 09:22:54 +0600

    almost done

--- lib/common_widgets/update_info_field.dart
+++ lib/common_widgets/update_info_field.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:mementum/utils/app_colors.dart';
 class UpdateInfoField extends StatelessWidget {
   final String tittle;
   final String hint;
@@ -14,7 +15,12 @@ class UpdateInfoField extends StatelessWidget {
           height: Get.height*0.05,
           width: double.infinity,
           decoration: BoxDecoration(
-            color: Colors.red
+            color: Colors.white,
+            border: Border.all(
+              width: 1,
+              color: AppColors.primarycolor
+            ),
+            borderRadius: BorderRadius.circular(12)
           ),
           child: Text(hint),
         ),
