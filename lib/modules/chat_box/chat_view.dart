7f8f0284ef8a0bc22eff026fb9506a5d836d3f44
rayhantmt <rayhantmt@gmail.com>
2025-10-14 08:29:15 +0600

    added padding in the stack

--- lib/modules/chat_box/chat_view.dart
+++ lib/modules/chat_box/chat_view.dart
@@ -21,32 +21,35 @@ class HistoryView extends GetView<ChatController> {
           Column(
             children: [
               SafeArea(
-                child: SizedBox(
-                  height: Get.height * 0.07,
-                  child: Stack(
-                    children: [
-                      Align(
-                        alignment: AlignmentGeometry.centerLeft,
-                        child: GestureDetector(
-                          onTap: () => Get.back(),
-                          child: Image.asset(
-                            AppImages.back_icon,
-                            height: Get.height * 0.07,
-                            width: Get.height * 0.07,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: SizedBox(
+                    height: Get.height * 0.07,
+                    child: Stack(
+                      children: [
+                        Align(
+                          alignment: AlignmentGeometry.centerLeft,
+                          child: GestureDetector(
+                            onTap: () => Get.back(),
+                            child: Image.asset(
+                              AppImages.back_icon,
+                              height: Get.height * 0.07,
+                              width: Get.height * 0.07,
+                            ),
                           ),
                         ),
-                      ),
-                      Center(
-                        child: Text(
-                          'Chatbox',
-                          style: GoogleFonts.inter(
-                            fontWeight: FontWeight.w500,
-                            fontSize: 18,
-                            color: Colors.black,
+                        Center(
+                          child: Text(
+                            'Chatbox',
+                            style: GoogleFonts.inter(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 18,
+                              color: Colors.black,
+                            ),
                           ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               ),
