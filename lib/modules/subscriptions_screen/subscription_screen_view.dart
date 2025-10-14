7667389ffaefc35b0247297b74c52f51be5324a9
rayhantmt <rayhantmt@gmail.com>
2025-10-15 05:40:17 +0600

    added the app bar

--- lib/modules/subscriptions_screen/subscription_screen_view.dart
+++ lib/modules/subscriptions_screen/subscription_screen_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:mementum/utils/app_images.dart';
 
 class SubscriptionScreenView extends StatelessWidget {
@@ -13,7 +15,32 @@ class SubscriptionScreenView extends StatelessWidget {
           height: double.infinity,
           width: double.infinity,
           fit: BoxFit.cover,
-          )
+          ),
+            SizedBox(
+                height: Get.height * 0.07,
+                child: Stack(
+                  children: [
+                    Align(
+                      alignment: AlignmentGeometry.centerLeft,
+                      child: Image.asset(
+                        AppImages.back_icon,
+                        height: Get.height * 0.07,
+                        width: Get.height * 0.07,
+                      ),
+                    ),
+                    Center(
+                      child: Text(
+                        'My Profile',
+                        style: GoogleFonts.inter(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 18,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
         ],
       ),
     );
