aaf8746016cfe85ac18226ef0dcef84d10125d35
rayhantmt <rayhantmt@gmail.com>
2025-10-15 05:30:28 +0600

    designing subscription

---
+++ lib/modules/subscriptions_screen/subscription_screen_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SubscriptionScreenView extends StatelessWidget {
+  const SubscriptionScreenView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
