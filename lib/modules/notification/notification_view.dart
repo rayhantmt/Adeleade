03b70fb69986ba105a3738099948404a0985f4d0
rayhantmt <rayhantmt@gmail.com>
2025-10-15 00:06:48 +0600

    added the app bar

--- lib/modules/notification/notification_view.dart
+++ lib/modules/notification/notification_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:mementum/utils/app_images.dart';
 
 class NotificationView extends StatelessWidget {
@@ -17,7 +19,31 @@ class NotificationView extends StatelessWidget {
           SafeArea(
             child: Column(
               children: [
-                
+                SizedBox(
+                height: Get.height * 0.07,
+                child: Stack(
+                  children: [
+                    Align(
+                      alignment: AlignmentGeometry.centerLeft,
+                      child: Image.asset(
+                        AppImages.back_icon,
+                        height: Get.height * 0.07,
+                        width: Get.height * 0.07,
+                      ),
+                    ),
+                    Center(
+                      child: Text(
+                        'My Profile',
+                        style: GoogleFonts.inter(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 18,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
               ],
             ),
           )
