2bad3eb3b680f73b9d4b1e4e9e432b921de96842
rayhantmt <rayhantmt@gmail.com>
2025-10-14 08:08:11 +0600

    made the code look good

--- lib/modules/main_screen/main_screen_binding.dart
+++ lib/modules/main_screen/main_screen_binding.dart
@@ -1,16 +1,15 @@
-
 import 'package:get/get.dart';
 import 'package:mementum/modules/chat_box/chat_controller.dart';
 import 'package:mementum/modules/home/home_controller.dart';
 import 'package:mementum/modules/main_screen/main_screen_controller.dart';
 import 'package:mementum/modules/memories/memories_controller.dart';
 
-class MainScreenBinding extends Bindings{
+class MainScreenBinding extends Bindings {
   @override
   void dependencies() {
-   Get.lazyPut<MainScreenController>(() => MainScreenController(),);
-    Get.lazyPut<HomeController>(() => HomeController(),);
-       Get.lazyPut<MemoriesController>(() => MemoriesController(),);
-       Get.lazyPut<ChatController>(() => ChatController(),);
+    Get.lazyPut<MainScreenController>(() => MainScreenController());
+    Get.lazyPut<HomeController>(() => HomeController());
+    Get.lazyPut<MemoriesController>(() => MemoriesController());
+    Get.lazyPut<ChatController>(() => ChatController());
   }
 }
