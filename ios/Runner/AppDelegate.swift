5817ca97c184dfaf518b739c69fe1adf097c91c6
rayhantmt <rayhantmt@gmail.com>
2025-10-15 03:42:29 +0600

    changed the appdelegete file and skipping the maps need api key from client

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,12 +1,15 @@
 import Flutter
 import UIKit
+import GoogleMaps // Add this import
 
-@main
+@UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // Set your API Key here
+    GMSServices.provideAPIKey("YOUR_GOOGLE_MAPS_API_KEY") 
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
