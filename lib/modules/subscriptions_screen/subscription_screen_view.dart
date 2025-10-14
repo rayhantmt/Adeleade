a0599fccc4b55a0179ae3b72e3c826a7dead1545
rayhantmt <rayhantmt@gmail.com>
2025-10-15 05:53:36 +0600

    gdfh

--- lib/modules/subscriptions_screen/subscription_screen_view.dart
+++ lib/modules/subscriptions_screen/subscription_screen_view.dart
@@ -11,54 +11,55 @@ class SubscriptionScreenView extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
-          Image.asset(AppImages.background,
-          height: double.infinity,
-          width: double.infinity,
-          fit: BoxFit.cover,
+          Image.asset(
+            AppImages.background,
+            height: double.infinity,
+            width: double.infinity,
+            fit: BoxFit.cover,
           ),
-            SafeArea(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Column(
-                  children: [
-                    SizedBox(
-                        height: Get.height * 0.07,
-                        child: Stack(
-                          children: [
-                            Align(
-                              alignment: AlignmentGeometry.centerLeft,
-                              child: Image.asset(
-                                AppImages.back_icon,
-                                height: Get.height * 0.07,
-                                width: Get.height * 0.07,
-                              ),
-                            ),
-                            Center(
-                              child: Text(
-                                'Subscription',
-                                style: GoogleFonts.inter(
-                                  fontWeight: FontWeight.w500,
-                                  fontSize: 18,
-                                  color: Colors.black,
-                                ),
-                              ),
-                            ),
-                          ],
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: Get.height * 0.07,
+                    child: Stack(
+                      children: [
+                        Align(
+                          alignment: AlignmentGeometry.centerLeft,
+                          child: Image.asset(
+                            AppImages.back_icon,
+                            height: Get.height * 0.07,
+                            width: Get.height * 0.07,
+                          ),
                         ),
-                      ),
-SizedBox(height: Get.height*0.07,),
-Text('Upgrade To Premium',
-style: GoogleFonts.inter(
-  fontWeight: FontWeight.w500,
-  fontSize: 24,
-  color: Colors.black
-),
-),
-
-                  ],
-                ),
+                        Center(
+                          child: Text(
+                            'Subscription',
+                            style: GoogleFonts.inter(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 18,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: Get.height * 0.07),
+                  Text(
+                    'Upgrade To Premium',
+                    style: GoogleFonts.inter(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 24,
+                      color: Colors.black,
+                    ),
+                  ),
+                ],
               ),
             ),
+          ),
         ],
       ),
     );
