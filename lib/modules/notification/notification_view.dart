b51dc4b3ff8cd7aab33f546fdf568683e43bf9ec
rayhantmt <rayhantmt@gmail.com>
2025-10-15 00:25:58 +0600

    removed an unused import

--- lib/modules/notification/notification_view.dart
+++ lib/modules/notification/notification_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:mementum/routes/app_pages.dart';
 import 'package:mementum/utils/app_images.dart';
 
 class NotificationView extends StatelessWidget {
