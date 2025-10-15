9d967b184a7198b10eacd9cf3eed24b21e48ce6c
rayhantmt <rayhantmt@gmail.com>
2025-10-16 03:39:39 +0600

    created event details page

---
+++ lib/modules/event_details/event_details.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class EventDetails extends StatelessWidget {
+  const EventDetails({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
