1d74ac02a51c303ce12398a80ab32eefac3bc9b2
rayhantmt <rayhantmt@gmail.com>
2025-10-16 00:49:25 +0600

    added back icon and text on forgot password page

--- lib/modules/forgot_password/forgot_password_view.dart
+++ lib/modules/forgot_password/forgot_password_view.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:mementum/common_widgets/textfield.dart';
 import 'package:mementum/routes/app_pages.dart';
 import 'package:mementum/utils/app_colors.dart';
+import 'package:mementum/utils/app_images.dart';
 
 class ForgotPasswordView extends StatelessWidget {
   const ForgotPasswordView({super.key});
@@ -11,72 +13,102 @@ class ForgotPasswordView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xffFAF2ED),
-      body: Padding(
-        padding: const EdgeInsets.only(left: 20, right: 20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(width: double.infinity),
-            Text(
-              "Momentum",
-              style: TextStyle(
-                fontWeight: FontWeight.w700,
-                fontSize: 32,
-                color: Colors.black,
-              ),
-            ),
-            SizedBox(height: Get.height * 0.033),
-            Text(
-              'Forgot password',
-              style: TextStyle(
-                fontWeight: FontWeight.w700,
-                fontSize: 26,
-                color: Colors.black,
-              ),
-            ),
-            SizedBox(height: Get.height * 0.033),
-            Text(
-              'Enter your email account to reset  your password',
-              maxLines: 6,
-              style: TextStyle(
-                fontWeight: FontWeight.w400,
-                fontSize: 16,
-                color: Color(0xff7D848D),
-              ),
-            ),
-            SizedBox(height: Get.height * 0.033),
-
-            Padding(
-              padding: EdgeInsets.only(left: Get.width * 0.1),
-              child: Row(children: [Text('Email')]),
-            ),
-            SizedBox(height: Get.height * 0.01),
-            Commontextfield(tittle: 'Rayhantmt@gmail.com', obsecuretext: false),
-            SizedBox(height: Get.height * 0.033),
-            GestureDetector(
-              onTap: () => Get.toNamed(AppPages.otppage),
-              child: Container(
+      body: Stack(
+        
+        children: [
+          SizedBox(
                 height: Get.height * 0.07,
-                width: Get.width * 0.8,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(99),
-                  color: AppColors.primarycolor,
+                child: Stack(
+                  children: [
+                    Align(
+                      alignment: AlignmentGeometry.centerLeft,
+                      child: Image.asset(
+                        AppImages.back_icon,
+                        height: Get.height * 0.07,
+                        width: Get.height * 0.07,
+                      ),
+                    ),
+                    Center(
+                      child: Text(
+                        'My Profile',
+                        style: GoogleFonts.inter(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 18,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-                child: Center(
-                  child: Text(
-                    'Send otp',
-                    style: TextStyle(
-                      fontWeight: FontWeight.w600,
-                      fontSize: 14,
-                      color: Colors.black,
+              ),
+          Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(width: double.infinity),
+                Text(
+                  "Momentum",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 32,
+                    color: Colors.black,
+                  ),
+                ),
+                SizedBox(height: Get.height * 0.033),
+                Text(
+                  'Forgot password',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 26,
+                    color: Colors.black,
+                  ),
+                ),
+                SizedBox(height: Get.height * 0.033),
+                Text(
+                  'Enter your email account to reset  your password',
+                  maxLines: 6,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 16,
+                    color: Color(0xff7D848D),
+                  ),
+                ),
+                SizedBox(height: Get.height * 0.033),
+          
+                Padding(
+                  padding: EdgeInsets.only(left: Get.width * 0.1),
+                  child: Row(children: [Text('Email')]),
+                ),
+                SizedBox(height: Get.height * 0.01),
+                Commontextfield(tittle: 'Rayhantmt@gmail.com', obsecuretext: false),
+                SizedBox(height: Get.height * 0.033),
+                GestureDetector(
+                  onTap: () => Get.toNamed(AppPages.otppage),
+                  child: Container(
+                    height: Get.height * 0.07,
+                    width: Get.width * 0.8,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(99),
+                      color: AppColors.primarycolor,
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Send otp',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 14,
+                          color: Colors.black,
+                        ),
+                      ),
                     ),
                   ),
                 ),
-              ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
