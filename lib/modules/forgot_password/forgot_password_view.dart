e8bb41c638c9e1b426e70389f6c4970560dbc6f7
rayhantmt <rayhantmt@gmail.com>
2025-10-16 00:50:30 +0600

    trying an another approach

--- lib/modules/forgot_password/forgot_password_view.dart
+++ lib/modules/forgot_password/forgot_password_view.dart
@@ -16,31 +16,7 @@ class ForgotPasswordView extends StatelessWidget {
       body: Stack(
         
         children: [
-          SizedBox(
-                height: Get.height * 0.07,
-                child: Stack(
-                  children: [
-                    Align(
-                      alignment: AlignmentGeometry.centerLeft,
-                      child: Image.asset(
-                        AppImages.back_icon,
-                        height: Get.height * 0.07,
-                        width: Get.height * 0.07,
-                      ),
-                    ),
-                    Center(
-                      child: Text(
-                        'My Profile',
-                        style: GoogleFonts.inter(
-                          fontWeight: FontWeight.w500,
-                          fontSize: 18,
-                          color: Colors.black,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+          
           Padding(
             padding: const EdgeInsets.only(left: 20, right: 20),
             child: Column(
