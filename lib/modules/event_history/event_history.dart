1421cd2ba234d5c7bf1014ac927f47ae81d51673
rayhantmt <rayhantmt@gmail.com>
2025-10-16 03:32:13 +0600

    added the event history page

--- lib/modules/event_history/event_history.dart
+++ lib/modules/event_history/event_history.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:mementum/modules/home/home_controller.dart';
 import 'package:mementum/utils/app_images.dart';
 
 class EventHistory extends StatelessWidget {
@@ -8,6 +9,7 @@ class EventHistory extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final controller=Get.find<HomeController>();
     return Scaffold(
       body: Stack(
         children: [
@@ -46,6 +48,106 @@ class EventHistory extends StatelessWidget {
                           ],
                         ),
                       ),
+
+                       Expanded(
+                  child: Obx(
+                    () => GridView.builder(
+                      padding: EdgeInsets.zero,
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                      ),
+                      itemCount: controller.events.length,
+
+                      itemBuilder: (context, index) {
+                        final data = controller.events[index];
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            height: Get.height * 0.2,
+                            decoration: BoxDecoration(
+                              color: Color(0xffF4EFE3B2).withOpacity(0.3),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                ClipRRect(
+                                  borderRadius: BorderRadiusGeometry.only(
+                                    topLeft: Radius.circular(8),
+                                    topRight: Radius.circular(8),
+                                  ),
+                                  child: Image.asset(
+                                    data.image,
+                                    height: Get.height * 0.1,
+                                    width: double.infinity,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                               // SizedBox(height: 5),
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 7),
+                                  child: Text(
+                                    data.tittle,
+                                    textAlign: TextAlign.start,
+                                    style: GoogleFonts.inter(
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 14,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 7),
+                                  child: Row(
+                                    children: [
+                                      Image.asset(
+                                        AppImages.calander_icon,
+                                        height: Get.height * 0.03,
+                                        width: Get.width * 0.05,
+                                      ),
+                                     // SizedBox(width: Get.width * 0.01),
+                                      Text(
+                                        data.date_time,
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.w400,
+                                          fontSize: 12,
+                                          color: Color(0xff898989),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 7),
+                                  child: Row(
+                                    children: [
+                                      Image.asset(
+                                        AppImages.location_icon,
+                                        height: Get.height * 0.03,
+                                        width: Get.width * 0.05,
+                                      ),
+                                      SizedBox(width: Get.width * 0.01),
+                                      Text(
+                                        data.location,
+
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.w400,
+                                          fontSize: 12,
+                                          color: Color(0xff898989),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ),
                  ],
                ),
              ),
