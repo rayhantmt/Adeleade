132e8b07cf8d9ab0860c812feacd0f750cbd395a
rayhantmt <rayhantmt@gmail.com>
2025-10-18 03:56:01 +0600

    created the controller

---
+++ lib/modules/chat_details/chat_details_controller.dart
@@ -0,0 +1,11 @@
+import 'package:get/get.dart';
+
+class ChatDetailsController extends GetxController{
+  late String img;
+  late String name;
+  @override
+  void onInit() {
+    
+    super.onInit();
+  }
+}
