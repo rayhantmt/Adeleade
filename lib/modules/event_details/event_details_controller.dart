7802e12c809ad4ce22c302c5dededd252cc7475b
rayhantmt <rayhantmt@gmail.com>
2025-10-19 00:34:48 +0600

    added the logic in the controller

--- lib/modules/event_details/event_details_controller.dart
+++ lib/modules/event_details/event_details_controller.dart
@@ -1,9 +1,13 @@
 import 'package:get/get.dart';
 
 class EventDetailsController extends GetxController{
+  late String image;
+  late String tittle;
   @override
   void onInit() {
-    
     super.onInit();
+    var arg=Get.arguments;
+     image =arg['image'];
+     tittle=arg['tittle'];
   }
 }
