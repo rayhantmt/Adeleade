f1ec27b3f4c35580e2644a78149a3690ac0a46ce
rayhantmt <rayhantmt@gmail.com>
2025-10-25 23:16:55 +0600

    added the text on event details screen

--- lib/modules/event_details/event_details_view.dart
+++ lib/modules/event_details/event_details_view.dart
@@ -76,41 +76,157 @@ class EventDetails extends GetView<EventDetailsController> {
                       ],
                     ),
                   ],
-               
-               
-                
-                
                 ),
-                SizedBox(height: Get.height*0.02,),
+                SizedBox(height: Get.height * 0.02),
                 Padding(
-                  padding: const EdgeInsets.only(left: 20,right: 20),
+                  padding: const EdgeInsets.only(left: 20, right: 20),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.start,
                     children: [
                       Column(
                         mainAxisAlignment: MainAxisAlignment.start,
                         children: [
-                          
-                          Text('Date and time',
-                          style: GoogleFonts.inter(
-                            fontWeight: FontWeight.w500,
-                            fontSize: 16,
-                            color: Color(0xff898989)
+                          Text(
+                            'Date and time',
+                            style: GoogleFonts.inter(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 16,
+                              color: Color(0xff898989),
+                            ),
                           ),
-                          ),
-                          Text(controller.time,
-                          style: GoogleFonts.inter(fontWeight: FontWeight.w500,
-                          fontSize: 16,
-                          color: Color(0xff1E1E1E)
-                          ),
+                          Text(
+                            controller.time,
+                            style: GoogleFonts.inter(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 16,
+                              color: Color(0xff1E1E1E),
+                            ),
                           ),
                         ],
                       ),
                     ],
                   ),
-                )
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20, right: 20),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Location",
+                            style: GoogleFonts.inter(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 18,
+                              color: Color(0xff1E1E1E),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Dhaka,Bangladesh',
+                            style: GoogleFonts.inter(
+                              fontWeight: FontWeight.w400,
+                              fontSize: 16,
+                              color: Color(0xff898989),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: Get.height * 0.015),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20, right: 20),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text(
+                            'About this activity',
+                            style: GoogleFonts.inter(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 16,
+                              color: Color(0xff1E1E1E),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Text(
+                        'Join us for an afternoon of golf combined with meaningful business networking. Perfect for professionals looking to expand',
+                        style: GoogleFonts.inter(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 16,
+                          color: Color(0xff898989),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: Get.height * 0.01),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20, right: 20),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            'Hosted by',
+                            textAlign: TextAlign.start,
+                            style: GoogleFonts.inter(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 18,
+                              color: Color(0xff1E1E1E),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20, right: 20),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Container(
+                            height: Get.height * 0.1,
+                            width: Get.width * 0.2,
+                            child: ClipRRect(
+                              borderRadius: BorderRadiusGeometry.circular(200),
+                              child: Image.asset(
+                                AppImages.profilephoto,
+                                fit: BoxFit.cover,
+                                height: Get.height * 0.1,
+                                width: Get.width * 0.2,
+                              ),
+                            ),
+                          ),
+                          SizedBox(width: Get.width * 0.05),
+                          Text(
+                            'Mostafa Rahma',
+                            style: GoogleFonts.inter(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 16,
+                              color: Color(0xff1E1E1E),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
               ],
-            
             ),
           ),
         ],
