3995a21edc00a141b2afbefaf70d4bf0500c1f4b
rayhantmt <rayhantmt@gmail.com>
2025-10-26 03:46:41 +0600

    made the appbar of the profile details screen

--- lib/modules/profile_details/Profile_details.dart
+++ lib/modules/profile_details/Profile_details.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:mementum/utils/app_images.dart';
 
 class ProfileDetails extends StatelessWidget {
   const ProfileDetails({super.key});
@@ -6,7 +9,46 @@ class ProfileDetails extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.red,
+      body: Stack(
+        children: [
+          Image.asset(AppImages.background,
+          height: double.infinity,
+          width: double.infinity,
+          fit: BoxFit.cover,
+          ),
+
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 20,right: 20),
+              child: SizedBox(
+                    height: Get.height * 0.07,
+                    child: Stack(
+                      children: [
+                        Align(
+                          alignment: AlignmentGeometry.centerLeft,
+                          child: Image.asset(
+                            AppImages.back_icon,
+                            height: Get.height * 0.07,
+                            width: Get.height * 0.07,
+                          ),
+                        ),
+                        Center(
+                          child: Text(
+                            'Profile',
+                            style: GoogleFonts.inter(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 18,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
