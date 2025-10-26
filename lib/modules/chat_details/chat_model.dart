02956fbf138d703930087196ca8de2e071b2d9b1
rayhantmt <rayhantmt@gmail.com>
2025-10-27 03:50:49 +0600

    created the dummy data model for the chatting screen

---
+++ lib/modules/chat_details/chat_model.dart
@@ -0,0 +1,9 @@
+class ChatModel {
+  final String msg;
+  final String time;
+  final String img;
+  final bool isme;
+
+  ChatModel({required this.msg, required this.time, required this.img, required this.isme});
+  
+}
