3c4ee520fa22c79b407bc38de90cb154bd288a6c
rayhantmt <rayhantmt@gmail.com>
2025-11-06 00:51:45 +0600

    ready for api integrations

---
+++ lib/api/api_config.dart
@@ -0,0 +1,19 @@
+class ApiConfig {
+  static const String baseUrl = 'https://7ee82e189612.ngrok-free.app/api/v1';
+
+  // Auth Endpoints
+  static const String loginEndpoint = '/auth/login';
+  static const String signupEndpoint = '/auth/signup';
+
+  // User Endpoints
+  static const String forgetpassword = '/auth/forgot_password';
+  static const String otpendpoint = '/auth/verify_otp';
+  static const String restpasswordendpoint='/auth/reset_password';
+  static const String changepasswordendpoint='/auth/update_password';
+  static const String psotappointment='/Appointment';
+  static const String getAppointmentEndpoint='/Appointment';
+  static const String track='/track';
+  static const String trackhistory='/track?trackType=';
+
+  // Other modules...
+}
