d700dc21c35787db0a8a1f1f11d13ea662680aa0
rayhantmt <rayhantmt@gmail.com>
2025-11-19 03:22:11 +0600

    added the update password api endpoint\

--- lib/api/api_config.dart
+++ lib/api/api_config.dart
@@ -5,5 +5,6 @@ class ApiConfig {
   static const String loginEndpoint = '/api/v1/user/login';
   static const String forgotpasswordendpoint='/api/v1/user/forgot-password';
   static const String verifyOtpendpoint='/api/v1/user/verify-reset-otp';
+  static const String updatepassword='/api/v1/user/reset-password';
 
 }
