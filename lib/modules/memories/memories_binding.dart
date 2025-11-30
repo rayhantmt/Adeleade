095a8e2be4463ceabf5a499565095a50ac58c1e3
rayhantmt <rayhantmt@gmail.com>
2025-12-01 00:26:19 +0600

    added the memories contorller in binding

--- lib/modules/memories/memories_binding.dart
+++ lib/modules/memories/memories_binding.dart
@@ -1,9 +1,10 @@
 import 'package:get/get.dart';
+import 'package:mementum/modules/memories/memories_controller.dart';
 
 
 class MemoriesBinding extends Bindings{
   @override
   void dependencies() {
-
+Get.lazyPut<MemoriesController>(() => MemoriesController(),);
   }
 }
