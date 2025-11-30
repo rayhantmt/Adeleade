40d66ac37b85ed0e90ab81dcf32fba8b6cce1381
rayhantmt <rayhantmt@gmail.com>
2025-12-01 03:33:02 +0600

    some issue with bearer token

--- lib/modules/memories/memories_controller.dart
+++ lib/modules/memories/memories_controller.dart
@@ -53,9 +53,7 @@ class MemoriesController extends GetxController {
     }
 
     try {
-      // 2. Prepare Gallery Files
-
-      // 3. Create FormData
+      
       final formData = FormData.fromMap({
         'name': namecontroller.text.toString(),
         'eventDate': eventdatecontroller.text.toString(),
@@ -69,13 +67,13 @@ class MemoriesController extends GetxController {
           contentType: MediaType('image', 'png'),
         ),
       });
-
+print('Bearer${token}');
       // 4. API Call
       final response = await _client.postFormData(
         url: '${ApiConfig.baseUrl}/api/v1/event/create',
         data: formData,
         headers: {
-          'Authorization': 'Bearer ${token}',
+          'Authorization': 'Bearer    ${token}',
           'Content-type': 'multipart/form-data',
         },
       );
