5dd1939fedee3692ffa4f16a23e7e7372c589381
rayhantmt <rayhantmt@gmail.com>
2025-12-01 00:32:15 +0600

    ceated the event image pickig function

--- lib/modules/memories/memories_controller.dart
+++ lib/modules/memories/memories_controller.dart
@@ -1,9 +1,31 @@
+import 'dart:io';
+
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 
 class MemoriesController extends GetxController{
   var memorytype = 0.obs; // 0 = On-Time, 1 = Recurring Deposit
-
+Rxn<XFile> eventImage=Rxn<XFile>();
   void setDepositType(int type) {
     memorytype.value = type;
+  }
+  final ImagePicker _picker=ImagePicker();
+  Future pickEventImage()async{
+   try {
+      final XFile? picked = await _picker.pickImage(
+        source: ImageSource.gallery,
+      );
+      if (picked != null) {
+        eventImage.value = picked;
+        eventImage.refresh();
+        print('Event Image picked: ${picked.path}');
+        Get.snackbar('Event image', picked.path);
+      } else {
+        print('Cover pick cancelled');
+      }
+    } catch (e) {
+      print('Error picking event image: $e');
+      Get.snackbar('Error', 'Failed to pick event image: $e');
+    }
   }
 }
