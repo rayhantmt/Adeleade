b888c48cb885ce6f269a7ce9878b76e07789f216
rayhantmt <rayhantmt@gmail.com>
2025-12-04 02:46:38 +0600

    created the controller hard codeed

--- lib/api/api_config.dart
+++ lib/api/api_config.dart
@@ -6,5 +6,6 @@ class ApiConfig {
   static const String forgotpasswordendpoint='/api/v1/user/forgot-password';
   static const String verifyOtpendpoint='/api/v1/user/verify-reset-otp';
   static const String updatepassword='/api/v1/user/reset-password';
+  static const String updateprofile='/api/v1/user/update-profile';
 
 }
--- lib/modules/update_info/update_info_controller.dart
+++ lib/modules/update_info/update_info_controller.dart
@@ -1,3 +1,31 @@
 import 'package:get/get.dart';
+import 'package:mementum/api/api_config.dart';
+import 'package:mementum/api/api_services.dart';
+import 'package:mementum/core/exceptions.dart';
 
-class UpdateInfoController extends GetxController {}
+class UpdateInfoController extends GetxController {
+  RxBool isloadin = false.obs;
+  Future updateprofile() async {
+    isloadin.value = true;
+    try {
+      final response = ApiService.patch(
+        endpoint: ApiConfig.updateprofile,
+        body: {
+          "name": "Test",
+          "bio": "bla bla bla",
+          "profession": "xyz",
+          "nationality": "Indian",
+          "instagram": "instagram-link",
+          "linkedIn": "linkedIn-link",
+        },
+      );
+      print('response $response');
+      Get.snackbar('Success', 'Congratulations profile updated successfully');
+    } on AppException catch (e) {
+      Get.snackbar('Updating profile failed ', e.toString());
+    }
+    finally{
+      isloadin.value=false;
+    }
+  }
+}
