5cee64d632d184673c5af30613a943a5ae9e04cf
rayhantmt <rayhantmt@gmail.com>
2025-12-04 03:03:01 +0600

    gdfsd

--- lib/modules/update_info/update_info_controller.dart
+++ lib/modules/update_info/update_info_controller.dart
@@ -1,9 +1,17 @@
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import 'package:mementum/api/api_config.dart';
 import 'package:mementum/api/api_services.dart';
 import 'package:mementum/core/exceptions.dart';
 
 class UpdateInfoController extends GetxController {
+  final namecontroller=TextEditingController();
+  final biocontroller=TextEditingController();
+  final professioncontroller=TextEditingController();
+  final nationalitycontroller=TextEditingController();
+  final instagramcontroller=TextEditingController();
+  final linkedincontroller=TextEditingController();
+
   RxBool isloadin = false.obs;
   Future updateprofile() async {
     isloadin.value = true;
