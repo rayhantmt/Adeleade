05494ab4d89e0441bd044710654eba3703342652
rayhantmt <rayhantmt@gmail.com>
2025-12-10 03:46:52 +0600

    removed some comment and added error messages proerly

--- lib/modules/home/home_controller.dart
+++ lib/modules/home/home_controller.dart
@@ -4,32 +4,9 @@ import 'package:mementum/api/api_config.dart';
 import 'package:mementum/api/api_services.dart';
 import 'package:mementum/core/exceptions.dart';
 import 'package:mementum/modules/home/home_model.dart';
-import 'package:mementum/utils/app_images.dart';
 
 class HomeController extends GetxController{
-//   var events=[
-// Event(tittle: "Gril's Night out", date_time: "20 Sept 2025, 5:00 PM", location: "Dhaka,Bangladesh", maxpeople: '1', joinedpeople: '7',image: AppImages.event1,),
-// Event(tittle: "Tenis", date_time: "20 Sept 2025, 5:00 PM", location: "Dhaka,Bangladesh", maxpeople: '15', joinedpeople: '7',image: AppImages.event2,),
-// Event(tittle: "Hiking Adventure", date_time: "20 Sept 2025, 5:00 PM", location: "Dhaka,Bangladesh", maxpeople: '15', joinedpeople: '7',image: AppImages.event3,),
-// Event(tittle: "Gala night", date_time: "20 Sept 2025, 5:00 PM", location: "Dhaka,Bangladesh", maxpeople: '17', joinedpeople: '7',image: AppImages.event4,),
-// Event(tittle: "Gril's Night out", date_time: "20 Sept 2025, 5:00 PM", location: "Dhaka,Bangladesh", maxpeople: '15', joinedpeople: '7',image: AppImages.event5,),
-// Event(tittle: "Gril's Night out", date_time: "20 Sept 2025, 5:00 PM", location: "Dhaka,Bangladesh", maxpeople: '164', joinedpeople: '7',image: AppImages.event6,),
-// Event(tittle: "Gril's Night out", date_time: "20 Sept 2025, 5:00 PM", location: "Dhaka,Bangladesh", maxpeople: '15', joinedpeople: '7',image: AppImages.event6,),
 
-// Event(tittle: "Gril's Night out", date_time: "20 Sept 2025, 5:00 PM", location: "Dhaka,Bangladesh", maxpeople: '15', joinedpeople: '7',image: AppImages.event6,),
-
-// Event(tittle: "Gril's Night out", date_time: "20 Sept 2025, 5:00 PM", location: "Dhaka,Bangladesh", maxpeople: '15', joinedpeople: '7',image: AppImages.event6,),
-// Event(tittle: "Gril's Night out", date_time: "20 Sept 2025, 5:00 PM", location: "Dhaka,Bangladesh", maxpeople: '15', joinedpeople: '7',image: AppImages.event6,),
-
-// Event(tittle: "Gril's Night out", date_time: "20 Sept 2025, 5:00 PM", location: "Dhaka,Bangladesh", maxpeople: '15', joinedpeople: '7',image: AppImages.event6,),
-
-// Event(tittle: "Gril's Night out", date_time: "20 Sept 2025, 5:00 PM", location: "Dhaka,Bangladesh", maxpeople: '15', joinedpeople: '7',image: AppImages.event6,),
-// Event(tittle: "Gril's Night out", date_time: "20 Sept 2025, 5:00 PM", location: "Dhaka,Bangladesh", maxpeople: '15', joinedpeople: '7',image: AppImages.event6,),
-// Event(tittle: "Gril's Night out", date_time: "20 Sept 2025, 5:00 PM", location: "Dhaka,Bangladesh", maxpeople: '15', joinedpeople: '7',image: AppImages.event6,),
-// Event(tittle: "Gril's Night out", date_time: "20 Sept 2025, 5:00 PM", location: "Dhaka,Bangladesh", maxpeople: '15', joinedpeople: '7',image: AppImages.event6,),
-
-
-//   ].obs;
 
   var ctgry=[
 Category(categories: 'All',),
@@ -106,13 +83,13 @@ var isLoading = false.obs;
     } on InternalServerException catch (e) {
       Get.snackbar(
         'Server Error',
-        'Something went wrong. Please try again later.',
+        'Something went wrong. $e Please try again later.',
         snackPosition: SnackPosition.BOTTOM,
       );
     } on FetchDataException catch (e) {
       Get.snackbar(
         'Network Error',
-        'Please check your internet connection',
+        'Please check your internet connection $e',
         snackPosition: SnackPosition.BOTTOM,
       );
     } catch (e) {
