6b8f3fa199b9ab173793199392e6a8b702ef80cc
rayhantmt <rayhantmt@gmail.com>
2025-12-15 00:16:19 +0600

    added the section on planned event

--- lib/modules/memories/memories_view.dart
+++ lib/modules/memories/memories_view.dart
@@ -264,132 +264,173 @@ Widget _plannedevent() {
   return Column(
     children: [
       Expanded(
-        child: Obx(
-          () => GridView.builder(
-            padding: EdgeInsets.zero,
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-            ),
-            itemCount: controller.events.length,
+                  child: Obx(
+                    () => GridView.builder(
+                      padding: EdgeInsets.zero,
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                      ),
+                      itemCount: controller.events.length,
 
-            itemBuilder: (context, index) {
-              final data = controller.events[index];
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: GestureDetector(
-                  onTap: () => Get.toNamed(
-                    AppPages.eventdetails,
-                    arguments: {
-                      'image': data.image,
-                      'tittle': data.title,
-                      'location': data.location,
-                      'time': data.formattedDate,
-                      'joinedpeople': data.joinedPeople,
-                    },
-                  ),
-                  child: Container(
-                    height: Get.height * 0.2,
-                    decoration: BoxDecoration(
-                      color: Color(0xffF4EFE3B2).withOpacity(0.3),
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    child: Stack(
-                      children: [
-                        Positioned(
-                          top: 50,
-                          right: 50,
-                          child: Container(
-                            height: 20,
-                            width: 20,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(8),
-                              color: Color(0xffF4EFE3),
+                      itemBuilder: (context, index) {
+                        final data = controller.events[index];
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: GestureDetector(
+                            onTap: () => Get.toNamed(
+                              AppPages.eventdetails,
+                              arguments: {
+                                'image': data.image,
+                                'tittle': data.title,
+                                'location': data.location,
+                                'time': data.formattedDate,
+                                'joinedpeople': data.joinedPeople,
+                                'eventDeatils':data.details,
+                                'hostedby':data.organizerName,
+                                'hostphotourl':data.organizerPhoto,
+                                'maxpeople':data.maxPeople
+                              },
                             ),
-                            child: Text(
-                              data.maxPeople,
-                              style: TextStyle(color: Colors.blue),
+                            child: Container(
+                              height: Get.height * 0.2,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  ClipRRect(
+                                    borderRadius: BorderRadiusGeometry.only(
+                                      topLeft: Radius.circular(8),
+                                      topRight: Radius.circular(8),
+                                    ),
+                                    child: Stack(
+                                      children: [
+                                        Image.network(
+                                          data.image,
+                                          height: Get.height * 0.1,
+                                          width: double.infinity,
+                                          fit: BoxFit.cover,
+                                        ),
+                                        Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.start,
+                                          children: [
+                                            Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.end,
+                                              children: [
+                                                Padding(
+                                                  padding:
+                                                      const EdgeInsets.only(
+                                                        right: 5,
+
+                                                        top: 5,
+                                                      ),
+                                                  child: Container(
+                                                    height: Get.height * 0.025,
+                                                    width: Get.width * 0.14,
+                                                    decoration: BoxDecoration(
+                                                      borderRadius:
+                                                          BorderRadius.circular(
+                                                            99,
+                                                          ),
+                                                      color: Colors.white,
+                                                    ),
+                                                    child: Row(
+                                                      children: [
+                                                        Image.asset(
+                                                          AppImages.personlogo,
+                                                          height:
+                                                              Get.height * 0.02,
+                                                          width:
+                                                              Get.width * 0.04,
+                                                        ),
+                                                        Text(
+                                                          '${data.joinedPeople}/${data.maxPeople}',
+                                                          style:
+                                                              GoogleFonts.inter(
+                                                                color: Colors
+                                                                    .black,
+                                                                fontWeight:
+                                                                    FontWeight
+                                                                        .w500,
+                                                                fontSize: 14,
+                                                              ),
+                                                        ),
+                                                      ],
+                                                    ),
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+
+                                  Padding(
+                                    padding: const EdgeInsets.only(left: 7),
+                                    child: Text(
+                                      data.title,
+                                      textAlign: TextAlign.start,
+                                      style: GoogleFonts.inter(
+                                        fontWeight: FontWeight.w500,
+                                        fontSize: 14,
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(left: 7),
+                                    child: Row(
+                                      children: [
+                                        Text(
+                                          data.title,
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.w400,
+                                            fontSize: 12,
+                                            color: Color(0xff898989),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(left: 7),
+                                    child: Row(
+                                      children: [
+                                        Image.asset(
+                                          AppImages.location_icon,
+                                          height: Get.height * 0.03,
+                                          width: Get.width * 0.05,
+                                        ),
+                                        SizedBox(width: Get.width * 0.01),
+                                        Text(
+                                          data.location,
+                                          overflow: TextOverflow.ellipsis,
+                                          maxLines: 1,
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.w400,
+                                            fontSize: 12,
+                                            color: Color(0xff898989),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
                             ),
                           ),
-                        ),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            ClipRRect(
-                              borderRadius: BorderRadiusGeometry.only(
-                                topLeft: Radius.circular(8),
-                                topRight: Radius.circular(8),
-                              ),
-                              child: Image.network(
-                                data.image,
-                                height: Get.height * 0.1,
-                                width: double.infinity,
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-
-                            // SizedBox(height: 5),
-                            Padding(
-                              padding: const EdgeInsets.only(left: 7),
-                              child: Text(
-                                data.title,
-                                textAlign: TextAlign.start,
-                                style: GoogleFonts.inter(
-                                  fontWeight: FontWeight.w500,
-                                  fontSize: 14,
-                                  color: Colors.black,
-                                ),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(left: 7),
-                              child: Row(
-                                children: [
-                                
-                                  Text(
-                                    data.formattedDate,
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.w400,
-                                      fontSize: 12,
-                                      color: Color(0xff898989),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(left: 7),
-                              child: Row(
-                                children: [
-                                  Image.asset(
-                                    AppImages.location_icon,
-                                    height: Get.height * 0.03,
-                                    width: Get.width * 0.05,
-                                  ),
-                                  SizedBox(width: Get.width * 0.01),
-                                  Text(
-                                    data.location,
-
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.w400,
-                                      fontSize: 12,
-                                      color: Color(0xff898989),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
+                        );
+                      },
                     ),
                   ),
                 ),
-              );
-            },
-          ),
-        ),
-      ),
     ],
   );
 }
