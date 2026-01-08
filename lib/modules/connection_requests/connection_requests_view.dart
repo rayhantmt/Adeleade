8bc909d9b2da5bb042e8219f01c607721792af2f
rayhantmt <rayhantmt@gmail.com>
2026-01-08 06:03:30 +0600

    changed some

--- lib/modules/connection_requests/connection_requests_view.dart
+++ lib/modules/connection_requests/connection_requests_view.dart
@@ -120,58 +120,62 @@ class ConnectionRequestsView extends GetView<ConnectionRequestController> {
                                           ),
                                           Row(
                                             children: [
-                                             InkWell(
-                                              onTap: () => controller
-                                                            .acceptConnection(
-                                                              controller
-                                                                  .requests[index]
-                                                                  .id,
-                                                            ),
-                                              child: Container(
-                                                height:
-                                                    Get.height * 0.03,
-                                                width:
-                                                    Get.width * 0.2,
-                                                decoration: BoxDecoration(
-                                                  color: AppColors
-                                                      .primarycolor,
-                                                  borderRadius:
-                                                      BorderRadius.circular(
-                                                        24,
+                                              InkWell(
+                                                onTap: () =>
+                                                    controller.acceptConnection(
+                                                      controller
+                                                          .requests[index]
+                                                          .id,
+                                                    ),
+                                                child: Container(
+                                                  height: Get.height * 0.03,
+                                                  width: Get.width * 0.2,
+                                                  decoration: BoxDecoration(
+                                                    color:
+                                                        AppColors.primarycolor,
+                                                    borderRadius:
+                                                        BorderRadius.circular(
+                                                          24,
+                                                        ),
+                                                  ),
+                                                  child: Center(
+                                                    child: Text(
+                                                      'Accept',
+                                                      style: GoogleFonts.inter(
+                                                        color: Colors.black,
+                                                        fontWeight:
+                                                            FontWeight.w400,
+                                                        fontSize: 14,
                                                       ),
-                                                ),
-                                                child: Center(
-                                                  child: Text(
-                                                    'Accept',
-                                                    style: GoogleFonts.inter(
-                                                      color: Colors
-                                                          .black,
-                                                      fontWeight:
-                                                          FontWeight
-                                                              .w400,
-                                                      fontSize: 14,
                                                     ),
                                                   ),
                                                 ),
                                               ),
-                                             ),
                                               SizedBox(width: Get.width * 0.05),
-                                              Container(
-                                                height: Get.height * 0.03,
-                                                width: Get.width * 0.2,
-                                                decoration: BoxDecoration(
-                                                  color: AppColors.primarycolor,
-                                                  borderRadius:
-                                                      BorderRadius.circular(24),
-                                                ),
-                                                child: Center(
-                                                  child: Text(
-                                                    'Decline',
-                                                    style: GoogleFonts.inter(
-                                                      color: Colors.red,
-                                                      fontWeight:
-                                                          FontWeight.w400,
-                                                      fontSize: 14,
+                                              InkWell(
+                                                onTap: () =>
+                                                    controller.removeconnection(
+                                                      controller
+                                                          .requests[index]
+                                                          .id,
+                                                    ),
+                                                child: Container(
+                                                  height: Get.height * 0.03,
+                                                  width: Get.width * 0.2,
+                                                  decoration: BoxDecoration(
+                                                    color: AppColors.primarycolor,
+                                                    borderRadius:
+                                                        BorderRadius.circular(24),
+                                                  ),
+                                                  child: Center(
+                                                    child: Text(
+                                                      'Decline',
+                                                      style: GoogleFonts.inter(
+                                                        color: Colors.red,
+                                                        fontWeight:
+                                                            FontWeight.w400,
+                                                        fontSize: 14,
+                                                      ),
                                                     ),
                                                   ),
                                                 ),
