fee3d1e292f32ca8de78762a676e18ae61182b72
rayhantmt <rayhantmt@gmail.com>
2026-01-25 04:03:26 +0600

    added successfully the cover image update

--- lib/modules/edit_profile/edit_profile_controller.dart
+++ lib/modules/edit_profile/edit_profile_controller.dart
@@ -1,4 +1,8 @@
-import 'package:get/get.dart';
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:get/get.dart' hide FormData, MultipartFile;
+import 'package:get_storage/get_storage.dart';
 import 'package:image_picker/image_picker.dart';
 
 class EditProfileController extends GetxController {
@@ -14,8 +18,38 @@ class EditProfileController extends GetxController {
       } else {
         Get.snackbar('Error', "Please select your profile image");
       }
+      _uploadAndSendMedia(File(profileImage.value!.path));
     } catch (e) {
       Get.snackbar('Error', e.toString());
+      print(e.toString());
     }
+  }
+}
+
+Future<void> _uploadAndSendMedia(File file) async {
+  try {
+    final dio = Dio();
+    final token = GetStorage().read('token');
+    final formdata = FormData.fromMap({
+      'cover': await MultipartFile.fromFile(file.path),
+    });
+    final response = await dio.patch(
+      options: Options(headers: {'Authorization': "Bearer $token"}),
+      data: formdata,
+      'https://server.momentumactivity.com/api/v1/user/update-cover-photo',
+    );
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      Get.snackbar(
+        'Success',
+        "Cover image updated restart the application to see the updates",
+      );
+      print(response.data.toString());
+    } else {
+      throw Get.snackbar('Error', response.data);
+      
+    }
+  } catch (e) {
+    Get.snackbar('Error', e.toString());
+    print(e.toString());
   }
 }
