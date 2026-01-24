6c55982995bad0018a19ccabcd215484f07a3c1e
rayhantmt <rayhantmt@gmail.com>
2026-01-25 02:37:15 +0600

    added the profile image picking

--- lib/modules/edit_profile/edit_profile_controller.dart
+++ lib/modules/edit_profile/edit_profile_controller.dart
@@ -1,3 +1,21 @@
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 
-class EditProfileController extends GetxController{} 
+class EditProfileController extends GetxController {
+  final ImagePicker picker = ImagePicker();
+  Rxn<XFile> profileImage = Rxn<XFile>();
+  Future<void> pickProfileImage() async {
+    try {
+      final XFile? pickedImage = await picker.pickImage(
+        source: ImageSource.gallery,
+      );
+      if (pickedImage != null) {
+        profileImage.value = pickedImage;
+      } else {
+        Get.snackbar('Error', "Please select your profile image");
+      }
+    } catch (e) {
+      Get.snackbar('Error', e.toString());
+    }
+  }
+}
