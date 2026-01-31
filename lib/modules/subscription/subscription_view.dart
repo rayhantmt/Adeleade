0877b2d2eafb24a98875dad06e9bc88b211695af
rayhantmt <rayhantmt@gmail.com>
2026-01-31 22:51:39 +0600

    done designing the page

--- lib/modules/subscription/subscription_view.dart
+++ lib/modules/subscription/subscription_view.dart
@@ -11,47 +11,75 @@ class SubscriptionView extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
-          Image.asset(AppImages.background,
-          height: double.infinity,
-          width: double.infinity,
-          fit: BoxFit.cover,
+          Image.asset(
+            AppImages.background,
+            height: double.infinity,
+            width: double.infinity,
+            fit: BoxFit.cover,
           ),
           Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              SizedBox(width: double.infinity,),
+              SizedBox(width: double.infinity),
               Container(
-                height: Get.height*0.6,
-                width: Get.width*0.8,
+                height: Get.height * 0.6,
+                width: Get.width * 0.8,
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(16),
-                  color: Colors.white
+                  color: Colors.white,
                 ),
                 child: Column(
                   children: [
-                    Image.asset(AppImages.logo,
-                    height: Get.height*0.07,
+                    Image.asset(AppImages.logo, height: Get.height * 0.07),
+                    Text(
+                      '    Quality over \nquantity, always',
+                      style: GoogleFonts.inter(
+                        fontWeight: FontWeight.w700,
+                        fontSize: 26,
+                      ),
                     ),
-                    Text('    Quality over \nquantity, always',
-                    style: GoogleFonts.inter( 
-                      fontWeight: FontWeight.w700,
-                      fontSize: 26
+                    SizedBox(height: Get.height * 0.04),
+                    Text(
+                      "Join Momentum's founding circle\n         of exceptional people",
+
+                      style: GoogleFonts.inter(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 16,
+                      ),
                     ),
+                    SizedBox(height: Get.height * 0.03),
+                    Divider(
+                      height: 1,
+                      indent: 20,
+                      endIndent: 20,
+                      color: Color(0xffDACBA4),
                     ),
-                    SizedBox(height: Get.height*0.04,),
-                    Text("Join Momentum's founding circle\n         of exceptional people",
-                    
-                    style: GoogleFonts.inter( 
-                      fontWeight: FontWeight.w400,
-                      fontSize: 16
+                    SizedBox(height: Get.height * 0.03),
+                    Container(
+                      height: Get.height * 0.05,
+                      width: Get.width * 0.7,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(100),
+                        color: Color(0xffDACBA4),
+                      ),
+                      child: Center(
+                        child: Text(
+                          'Secure founding Access for \$19',
+                          style: GoogleFonts.inter(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ),
                     ),
-                    )
+                    SizedBox(height: Get.height*0.03,),
+                    Text("You'll be cahrged once.If accepted,\n          your access is lifetime")
                   ],
                 ),
               ),
             ],
-          )
+          ),
         ],
       ),
     );
