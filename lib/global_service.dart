1c46015e7fa5ae4d74aa003d661aead3be10ac15
rayhantmt <rayhantmt@gmail.com>
2026-04-15 02:38:26 +0600

    formatted document

--- lib/global_service.dart
+++ lib/global_service.dart
@@ -1,18 +1,25 @@
 import 'package:get/get.dart';
 
-class GlobalService extends GetxController{
-  var name =''.obs;
-  var bio=''.obs;
-  var profession=''.obs;
-  var nationality=''.obs;
-  var instagram=''.obs;
-  var linkedin=''.obs;
-  void updaedinfo(String updatedname,String updtedbio,String updatedprofession,String upnationality,String updatedinstagram,String updatedlinkedin){
-name.value=updatedname;
-bio.value=updtedbio;
-profession.value=updatedprofession;
-nationality.value=upnationality;
-instagram.value=updatedinstagram;
-linkedin.value=updatedlinkedin;
+class GlobalService extends GetxController {
+  var name = ''.obs;
+  var bio = ''.obs;
+  var profession = ''.obs;
+  var nationality = ''.obs;
+  var instagram = ''.obs;
+  var linkedin = ''.obs;
+  void updaedinfo(
+    String updatedname,
+    String updtedbio,
+    String updatedprofession,
+    String upnationality,
+    String updatedinstagram,
+    String updatedlinkedin,
+  ) {
+    name.value = updatedname;
+    bio.value = updtedbio;
+    profession.value = updatedprofession;
+    nationality.value = upnationality;
+    instagram.value = updatedinstagram;
+    linkedin.value = updatedlinkedin;
   }
 }
