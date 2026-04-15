f9fddf30c121f4f7f8c1176c262a893c725414f4
rayhantmt <rayhantmt@gmail.com>
2026-04-16 03:59:37 +0600

    added the profile image global variable

--- lib/global_service.dart
+++ lib/global_service.dart
@@ -4,15 +4,19 @@ import 'package:get_storage/get_storage.dart';
 class GlobalService extends GetxController {
   @override
   void onInit() {
-readfromstorage();
+    readfromstorage();
     super.onInit();
   }
+
   var name = ''.obs;
   var bio = ''.obs;
   var profession = ''.obs;
   var nationality = ''.obs;
   var instagram = ''.obs;
   var linkedin = ''.obs;
+  var profileimage = ''.obs;
+  var coverimage = ''.obs;
+
   void updaedinfo(
     String updatedname,
     String updtedbio,
@@ -32,12 +36,20 @@ readfromstorage();
   void readfromstorage() {
     final storage = GetStorage();
     name.value = storage.read('name');
-
     profession.value = storage.read('profession');
     bio.value = storage.read('bio');
-
     nationality.value = storage.read('nationality');
     linkedin.value = storage.read('linkedIn');
     instagram.value = storage.read('instagram');
+    profileimage.value = storage.read('photoURL');
+    coverimage.value = storage.read('coverPhotoURL');
+  }
+
+  void updatepforileurl(String url) {
+    profileimage.value = url;
+  }
+
+  void updatecoverurl(String url) {
+    coverimage.value = url;
   }
 }
